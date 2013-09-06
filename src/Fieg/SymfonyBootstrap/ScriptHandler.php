32fa0ea29e7ad5e297e13b23eeaa9c6c57da290f
fieg <jeroen@financial-media.nl>
2013-09-06 10:27:58 +0200

    finalized scripthandler

--- src/Fieg/SymfonyBootstrap/ScriptHandler.php
+++ src/Fieg/SymfonyBootstrap/ScriptHandler.php
@@ -6,6 +6,7 @@ namespace Fieg\SymfonyBootstrap;
 
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\Finder\SplFileInfo;
 
 class ScriptHandler
 {
@@ -13,16 +14,28 @@ class ScriptHandler
     {
 //        $composer = $event->getComposer();
 
+        $rootDir = __DIR__ . '/../../..';
+
         $filesystem = new Filesystem();
         $finder = new Finder();
         $finder
-            ->in(__DIR__ . '/../../../vendor/symfony/framework-standard-edition')
+            ->in($rootDir . '/vendor/symfony/framework-standard-edition')
             ->depth(0)
             ->notName('composer.json')
+            ->ignoreDotFiles(false)
         ;
 
+        /** @var $file SplFileInfo */
         foreach($finder as $file) {
-            $filesystem->copy($file, __DIR__ . '/../../../');
+            $targetPath = $rootDir . '/' . $file->getFilename();
+
+            printf("Coping %s\n", 'vendor/symfony/framework-standard-edition/' . $file->getRelativePathname());
+
+            if ($file->isDir()) {
+                $filesystem->mirror($file, $targetPath);
+            } else {
+                $filesystem->copy($file, $targetPath);
+            }
         }
     }
 }
