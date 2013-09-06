cc27e2b4c281bb71b2e09ee4991deb17165ede76
fieg <jeroen@financial-media.nl>
2013-09-06 11:41:52 +0200

    added patches

--- src/Fieg/SymfonyBootstrap/ScriptHandler.php
+++ src/Fieg/SymfonyBootstrap/ScriptHandler.php
@@ -2,19 +2,24 @@
 
 namespace Fieg\SymfonyBootstrap;
 
-//use Composer\Script\Event;
+use Composer\Script\Event;
 
+use Composer\Json\JsonManipulator;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
+use Symfony\Component\Process\Process;
 
 class ScriptHandler
 {
-    public static function patch($event)
+    protected static function getRootDir()
     {
-//        $composer = $event->getComposer();
+        return __DIR__ . '/../../..';
+    }
 
-        $rootDir = __DIR__ . '/../../..';
+    public static function copyFramework(Event $event)
+    {
+        $rootDir = self::getRootDir();
 
         $filesystem = new Filesystem();
         $finder = new Finder();
@@ -37,5 +42,67 @@ class ScriptHandler
                 $filesystem->copy($file, $targetPath);
             }
         }
+    }
+
+    public static function applyPatches(Event $event)
+    {
+        $rootDir = self::getRootDir();
+
+        $finder = new Finder();
+        $finder->files()->in($rootDir . '/patches');
+
+        foreach($finder as $file) {
+            // printf("Patching %s (%s)\n", $file->getRelativePathname(), sprintf('patch -p0 < %s', $file->getPathname()));
+
+            $process = new Process(sprintf('patch -p0 < %s', $file->getPathname()), self::getRootDir());
+            $process->run();
+
+            echo $process->getOutput();
+        }
+
+    }
+
+    public static function patchComposerJson(Event $event)
+    {
+        $rootDir = self::getRootDir();
+
+        $standardComposerJson = $rootDir . '/vendor/symfony/framework-standard-edition/composer.json';
+        $composerJson = $rootDir . '/composer.json';
+
+        printf("Patching composer.json\n");
+
+        file_put_contents($composerJson, file_get_contents($standardComposerJson));
+
+        return;
+//
+//        $config = json_decode(file_get_contents($standardComposerJson), true);
+//        $manipulator = new JsonManipulator(file_get_contents($composerJson));
+//
+//        foreach($config['require'] as $package => $constraint) {
+//            $manipulator->addLink('require', $package, $constraint);
+//        }
+//
+//        foreach($config['scripts'] as $package => $constraint) {
+//            $manipulator->addLink('require', $package, $constraint);
+//        }
+//
+//        var_dump($config['scripts']); die(sprintf('Dump originated from %s on line %s', __FILE__, __LINE__));
+//
+//        file_put_contents($composerJson, $manipulator->getContents());
+    }
+
+    public static function runComposerUpdate(Event $event)
+    {
+        $composer = $event->getComposer();
+
+        list ($cmd) = $GLOBALS['argv'];
+
+        $process = new Process(sprintf('%s update', $cmd), self::getRootDir());
+        $process->run(function ($type, $data) {
+                echo $data;
+            }
+        );
+
+        echo $process->getOutput();
     }
 }
